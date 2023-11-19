dbe50f237eb9edd8bac01adf65fcacf4d47f517f
freerainboxbox <linmichael02@gmail.com>
2023-11-18 23:29:37 -0500

    TODO: connect to DB

--- machine-learning-client/ml_client.py
+++ machine-learning-client/ml_client.py
@@ -1,8 +1,12 @@
+import base64
+import base62
 from flask import Flask, request
+from os import getenv
+import whisper
 
 from bson.objectid import ObjectId
 
-oidtob62 = lambda oid: base62.encodebytes(oid.binary)
+oidtob62 = lambda oid: base64.encodebytes(oid.binary)
 b62tooid = lambda b62: ObjectId(base62.decodebytes(b62))
 
 def main():
@@ -11,8 +15,10 @@ def main():
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
     # Get object ID from request
-    object_id = request.form['object_id']
-
+    oid = b62tooid(request.form['id'])
+    # Get pickled opus audio data from database
+    pass # TODO: blocked on database connection details
+    
 
 if __name__ == '__main__':
     main()
