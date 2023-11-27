1de429d2157a17a790ea41408794f20a856702e6
ec3566 <47432136+ec3566@users.noreply.github.com>
2023-11-26 22:59:43 -0500

    barebones, just for the record to indicate start

---
+++ machine-learning-client/test_ml_client.py
@@ -0,0 +1,18 @@
+import pytest
+
+from ml_client import *
+
+def test_write_to_srt ( ):
+    pass
+
+def test_main ( ):
+    pass
+
+def test_transcribe_job ( ):
+    pass
+
+def test_transcribe ( ):
+    pass
+
+def test_index ( ):
+    pass
