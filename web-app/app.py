99f2090211a692f75a07ee66103b162b93acbcff
Seolin Jung <sj3182@nyu.edu>
2023-11-27 00:46:38 -0500

    added app.py, backend for web app

---
+++ web-app/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect
+
+from os import getenv
+from bson.objectid import ObjectId
+
+import os
+import base64
+import pymongo
+import datetime
+
+oidtob62 = lambda oid: base64.encodebytes(oid.binary)
+b62tooid = lambda b62: ObjectId(base64.decodebytes(b62))
+
+template_dir = os.path.abspath('/templates')
+static_dir = os.path.abspath('/static')
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+
+def main():
+    # connect to database
+    global db
+    db = pymongo.MongoClient("mongodb://mongo:27017",
+                            username=getenv("MONGO_USER"),
+                            password=getenv("MONGO_PASSWORD"),
+    )
+
+    app.run(ssl_context=('cert.pem', 'key.pem'))
+
+if __name__ == "__main__":
+    main()
