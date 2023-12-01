f532506583e9aafa33cc16bb99346ef7847b8ed5
Ethan Delgado <efd7152@nyu.edu>
2023-11-30 19:31:16 -0500

    changes made to use opus-recorder and send data to /upload

--- web-app/static/js/makeRecordings.js
+++ web-app/static/js/makeRecordings.js
@@ -1,19 +1,63 @@
+let recorder;
+let audioChunks = [];
+
 document.getElementById('startRecording').addEventListener('click', function() {
-    //to start recording
-    console.log('Recording started');
-    this.disabled = true;
-    document.getElementById('stopRecording').disabled = false;
+    Recorder.getAudioStreamAsync().then(function(stream) {
+        recorder = new Recorder({
+            encoderPath: "encoderWorker.min.js",
+            stream: stream
+        });
+
+        recorder.start().then(() => {
+            console.log('Recording started');
+            document.getElementById('startRecording').disabled = true;
+            document.getElementById('stopRecording').disabled = false;
+        });
+    }).catch(function(err) {
+        console.error('Error accessing audio stream:', err);
+    });
 });
 
 document.getElementById('stopRecording').addEventListener('click', function() {
-    //to stop recording
-    console.log('Recording stopped');
-    this.disabled = true;
-    document.getElementById('startRecording').disabled = false;
+    recorder.stop().then(({blob}) => {
+        window.audioBlob = blob;
+        console.log('Recording stopped');
+        document.getElementById('audioPlayback').src = URL.createObjectURL(blob);
+        document.getElementById('audioPlayback').style.display = 'block';
+        document.getElementById('stopRecording').disabled = true;
+        document.getElementById('startRecording').disabled = false;
+    });
 });
 
 document.getElementById('recordingForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    //to upload recording
-    console.log('Recording uploaded');
+
+    if (!window.audioBlob) {
+        alert("No recording available. Please record something first.");
+        return;
+    }
+
+    let formData = new FormData();
+    formData.append('audio.opus', window.audioBlob, 'audio.opus');
+    formData.append('username', document.getElementById('username').value);
+    formData.append('name', document.getElementById('recordingName').value);
+
+    fetch('/upload', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+      console.log('Success:', data);
+      alert("Recording uploaded successfully.");
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      alert("Failed to upload recording.");
+    });
 });
